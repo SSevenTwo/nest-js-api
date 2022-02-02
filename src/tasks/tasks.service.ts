229f50b05554bf34e78d85b38784e5d5d7cd3367
Ian Nguyen <s3788210@student.rmit.edu.au>
2022-02-02 19:44:17 +1100

    Added get end point

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { CreateTaskDto } from './create-task.dto';
+import { Task, TaskStatus } from './task.model';
+import { v4 } from 'uuid';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    private tasks: Task[] = [];
+
+    constructor(){
+        const task: Task = {
+            id: v4(),
+            title: "Clean house",
+            description: "All rooms are filthy.",
+            status: TaskStatus.OPEN
+        };
+
+        this.tasks.push(task);
+    }
+
+    getAllTasks(): Task[] {
+        return this.tasks;
+    }
+
+    createTask(createTaskDto:CreateTaskDto): Task{
+        const { title, description } = createTaskDto;
+
+        const task: Task = {
+            id: v4(),
+            title,
+            description,
+            status: TaskStatus.OPEN
+        };
+
+        this.tasks.push(task);
+        return task;
+    }
+}
