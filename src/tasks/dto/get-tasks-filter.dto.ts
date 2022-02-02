f3869ec3b8764c716e4de88e6b447aee875bc7b2
Ian Nguyen <s3788210@student.rmit.edu.au>
2022-02-02 22:31:55 +1100

    Added validation rules

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,13 @@
+import { IsNotEmpty , IsEnum, IsOptional, IsString } from "class-validator";
 import { TaskStatus } from "../task.model";
 
 export class GetTasksFilterDto{
+    @IsOptional()
+    @IsEnum(TaskStatus)
     status?:TaskStatus;
+
+    
+    @IsOptional()
+    @IsString()
     description?:string;
 }
